5e9c32ac11039b683f9b7d4890867e461d77e006
Alexandre Gattiker <algattik@microsoft.com>
2023-05-22 10:56:15 +0200

    .

---
+++ deployments/infrastructure/outputs.tf
@@ -0,0 +1,29 @@
+
+output "resource_group_name" {
+  value = module.rg.name
+}
+
+output "key_vault_name" {
+  value = module.keyvault.name
+}
+
+output "metastore_jdbc_connection_string" {
+  value = module.db.jdbc_connection_string
+}
+
+output "metastore_username_secret_name" {
+  value = module.db.username_secret_name
+}
+
+output "metastore_password_secret_name" {
+  value = module.db.password_secret_name
+}
+
+output "databricks_workspace_name" {
+  value = module.adb.name
+}
+
+output "databricks_workspace_host" {
+  value = module.adb.workspace_host
+}
+
---
+++ modules/keyvault/outputs.tf
@@ -0,0 +1,7 @@
+output "kv_id" {
+  value = azurerm_key_vault.adb_kv.id
+}
+
+output "name" {
+  value = azurerm_key_vault.adb_kv.name
+}
